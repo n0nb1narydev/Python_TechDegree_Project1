b2f873b80701d71a1d9ff4d71ca1be7da6affadf
Mel Rumsey <melsrumsey@gmail.com>
2020-09-14 14:11:00 -0500

    Complete

--- guessing_game.py
+++ guessing_game.py
@@ -9,21 +9,29 @@ def start_game(best_score):
     print("==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==\n      Welcome to the Number Guessing Game!\n==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==")
     
     answer = random.randint(1,10)
-    guess = int(input("Please enter a number between 1 and 10: "))
-    tries = 1
+    tries = 0
+    guess = 0
+    
 
     while guess != answer:
-        if guess < answer:
-            print("It is higher.")
-        elif guess > answer:
-            print("It is lower.")
-        tries += 1
-        guess = int(input("Try again. Please enter a number between 1 and 10: "))
-    
+        try:
+            guess = int(input("Please enter a number between 1 and 10: "))
+            if guess > 10 or guess < 1:
+                raise ValueError("Enter a valid number between 1 and 10")
+
+        except ValueError as err:
+            print("Enter a valid number between 1 and 10")
+        else:
+            if guess < answer:
+                print("It is higher.")
+            elif guess > answer:
+                print("It is lower.")
+            tries += 1    
+
     if guess == answer:
         print("You guessed the number in {} tries!".format(tries))
-        if best_score < tries:
-            print("The best score is {}".format(best_score))
+        if best_score <= tries:
+            print("The best score is {}.".format(best_score))
             restart_game(best_score)
         elif best_score > tries:
             print("Contratulations! You beat the best score of {}".format(best_score))
@@ -31,13 +39,13 @@ def start_game(best_score):
             restart_game(tries)
 
 def restart_game(best_score):
-    play_again = input("Would you like to play again? (y/n) ")
-    try:
+        play_again = input("Would you like to play again? (y/n) ")
         if play_again.lower() == "y":
             start_game(best_score)
-        if play_again.lower() == "n":
+        elif play_again.lower() == "n":
             print("Thanks for playing!")
-    except ValueError:
-        print("Oh no! You did not enter a Y or N")
+        else:
+            print("Please enter a Y or N.")
+            restart_game(best)
 
 start_game(best)
