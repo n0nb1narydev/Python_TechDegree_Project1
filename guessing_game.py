53f883bde10515d1c2e57ba2e26cf04bf462c749
Mel Rumsey <melsrumsey@gmail.com>
2020-09-14 11:39:42 -0500

    game save

--- guessing_game.py
+++ guessing_game.py
@@ -1,37 +1,39 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 
+# Python Web Development Techdegree
+# Project 1 - Number Guessing Game
+
+high_score = 10
 
 def start_game():
-    """Psuedo-code Hints
+    print("==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==\n      Welcome to the Number Guessing Game!\n==*==*==*==*==*==*==*==*==*==*==*==*==*==*==*==")
     
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-        a. If the guess greater than the solution, display to the player "It's lower".
-        b. If the guess is less than the solution, display to the player "It's higher".
+    answer = random.randint(1,10)
+    guess = int(input("Please enter a number between 1 and 10: "))
+    tries = 1
     
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-        and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+    while guess != answer:
+        if guess < answer:
+            print("It is higher.")
+            tries += 1
+        elif guess > answer:
+            print("It is lower.")
+            tries += 1
+        guess = int(input("Try again. Please enter a number between 1 and 10: "))
+    if guess == answer:
+            print("You guessed the number in {} tries!".format(tries))
+            if high_score < tries:
+                print("The best score is {}".format(high_score))
+            elif high_score > tries:
+                print("Contratulations! You beat the best score of {}".format(high_score))
+                high_score = tries
 
+def restart_game():
+    play_again = input("Would you like to play again? (y/n) ")
+    if play_again == "y":
+        start_game()
+    if play_again == "n":
+        print("Thanks for playing!")
 
-
-# Kick off the program by calling the start_game function.
 start_game()
+restart_game()
